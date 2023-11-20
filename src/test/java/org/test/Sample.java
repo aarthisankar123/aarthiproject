d15541d3c90add9513a06aa015f30065d4b9deb9
aarthisankar123 <aarthiece200@gmail.com>
2023-11-20 10:36:54 +0530

    Added code

--- src/test/java/org/test/Sample.java
+++ src/test/java/org/test/Sample.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class Sample {
 	@Test
 	private  void test(){
-		//starts your code here
+		
 		System.out.println("test 1");
 		System.out.println("test 11");
 		System.out.println("test 101");
